304406e91f490cb11e2b3d40cc75d10e294b4cb5
ShamimRahimi <119719139+ShamimRahimi@users.noreply.github.com>
2023-01-10 08:32:23 +0330

    Add files via upload
    added cat

--- main.c
+++ main.c
@@ -14,21 +14,53 @@ void createfile(){
     char* slash_filename = strrchr(command_backup2 , '/');
     char* filename=slash_filename+1;
    
-    if (access(filename, F_OK) == 0) {
-        printf("%s already exists" , filename);
-    } else {
-        FILE *fp;
-        fp=fopen(filename, "w");
-        fclose(fp);
-    }
+    
   
     if(val[0]==' ' && val[1]=='-' && val[2]=='-' && val[3]=='f' && val[4]=='i' && val[5]=='l' && val[6]=='e'){
         if(val2[0]==' ' && val[1]=='-' && val[2]=='-' && val[3]=='f' && val[4]=='i' && val[5]=='l'){
-            
+            if (access(filename, F_OK) == 0) {
+             printf("%s already exists" , filename);
+             }
+            else {
+            FILE *fp;
+            fp=fopen(filename, "w");
+            fclose(fp);
+            }
         }
     }
+    else{
+        printf("Invalid Command\n");
+    }
+}
 
-   
+void cat(){
+    char command_backup2[1000];
+    strcpy(command_backup2 , command);
+    char* val = strchr(command_backup2 , ' ');
+    char* slash_filename = strrchr(command_backup2 , ' ');
+    char* filename=slash_filename+1;
+    
+     if(val[0]==' ' && val[1]=='-' && val[2]=='-' && val[3]=='f' && val[4]=='i' && val[5]=='l' && val[6]=='e'){
+        FILE* ptr;
+        char ch;
+        ptr = fopen(filename, "r");
+ 
+            if (NULL == ptr) {
+            printf("file can't be opened \n");
+            }
+ 
+  
+            while (ch != EOF){
+                ch = fgetc(ptr);
+                printf("%c", ch);
+            }
+
+            fclose(ptr);
+    }
+    else{
+        printf("Invalid Command\n");
+    }
+    
 }
 
 int main(){
@@ -39,6 +71,10 @@ int main(){
     
     if( !strcmp (strtok(command_backup, " ") , "createfile")){
         createfile();
+    }
+
+    if( !strcmp (strtok(command_backup, " ") , "cat")){
+        cat();
     }
     
     return 0;
